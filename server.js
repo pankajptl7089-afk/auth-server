a31b0b530b6a0bc59c85188c1680157fe937b42d
pankajptl7089-afk <pankajptl7089@gmail.com>
2026-01-22 23:32:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
-// --- 1 Minute Testing Limit Check ---
-const today = new Date();
-const expiryDate = new Date(foundKey.createdAt);
+app.get('/verify-token', async (req, res) => {
+    const { token, deviceId } = req.query;
 
-// 30 din ki jagah 1 minute add karein
-expiryDate.setMinutes(expiryDate.getMinutes() + 1); 
+    try {
+        const foundKey = await Key.findOne({ keyCode: token });
+        if (!foundKey) return res.status(401).send("Invalid Token! Please check.");
 
-if (today > expiryDate) {
-    return res.status(403).send("Token Expired! 1 minute over.");
-}
-// -----------------------------
+        // --- 5 Minute Testing Logic ---
+        const today = new Date();
+        const expiryDate = new Date(foundKey.createdAt);
+        
+        // 1 ki jagah 5 minute add karein
+        expiryDate.setMinutes(expiryDate.getMinutes() + 5); 
+
+        if (today > expiryDate) {
+            return res.status(403).send("Token Expired! 5-minute limit over.");
+        }
+
+        // --- Device Binding Logic ---
+        if (!foundKey.deviceId || foundKey.deviceId === "" || foundKey.deviceId === "null") {
+            foundKey.deviceId = deviceId;
+            foundKey.isUsed = true;
+            await foundKey.save();
+            return res.status(200).send("Success");
+        }
+
+        if (foundKey.deviceId === deviceId) {
+            return res.status(200).send("Success");
+        } else {
+            // Iska popup bhi app mein "Used on another device!" dikhayega
+            return res.status(403).send("Used on another device!");
+        }
+
+    } catch (err) {
+        res.status(500).send("Server Error");
+    }
+});
